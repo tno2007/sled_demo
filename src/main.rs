d1514219d6ae281b02b5c22c32f5b26e78cc66bd
Lyall <lyall@iriesoft.net>
2025-07-10 21:06:50 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,137 @@
+#[macro_use]
+extern crate rocket;
+
+use dashmap::DashMap;
+// use rocket::serde::json::Json;
+use rocket::serde::{Serialize, json::Json};
+use rocket_okapi::{
+    openapi, openapi_get_routes,
+    swagger_ui::{SwaggerUIConfig, make_swagger_ui},
+};
+use schemars::JsonSchema;
+use serde::Deserialize;
+//use std::error::Error;
+
+// import ./src/modules/etf.rs module
+mod modules {
+    pub mod etf;
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Person {
+    id: u64,
+    name: String,
+    age: u8,
+    city: String,
+}
+
+#[derive(Serialize, Deserialize, JsonSchema)]
+struct Message {
+    text: String,
+}
+
+#[openapi]
+#[get("/")]
+fn index() -> Json<Message> {
+    Json(Message {
+        text: "Welcome to the ETF API!".to_string(),
+    })
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/api", openapi_get_routes![index])
+        .mount(
+            "/swagger",
+            make_swagger_ui(&SwaggerUIConfig {
+                url: "/api/openapi.json".to_owned(),
+                ..Default::default()
+            }),
+        )
+}
+
+/*
+fn main() {
+    // let file_path = "src/etfs.json"; // Adjust the path as needed
+    // let etfs = read_json_file(file_path).unwrap_or_default();
+    // println!("ETFs: {:#?}", etfs.len());
+
+    let csv_file_path = "src/etfs.csv"; // Adjust the path as needed
+    let etfs = modules::etf::read_etfs_from_csv(csv_file_path).unwrap_or_else(|err| {
+        eprintln!("Error reading ETFs from CSV: {}", err);
+        Vec::new()
+    });
+    println!("ETFs: {:#?}", etfs.len());
+
+    // print first 100 ETFs
+    for etf in etfs.iter().take(10) {
+        // print out etf minified
+        println!("{:?}", etf);
+
+        // print out etf in pretty format
+        //println!("{:#?}", etf);
+    }
+
+    let db: DashMap<u64, Person> = DashMap::new();
+
+    // 1️⃣ CREATE
+    let alice = Person {
+        id: 1,
+        name: "Alice".into(),
+        age: 30,
+        city: "Cape Town".into(),
+    };
+
+    let bob = Person {
+        id: 2,
+        name: "Bob".into(),
+        age: 25,
+        city: "Johannesburg".into(),
+    };
+
+    let lyall = Person {
+        id: 3,
+        name: "Lyall".into(),
+        age: 25,
+        city: "Johannesburg".into(),
+    };
+
+    db.insert(alice.id, alice.clone());
+    db.insert(bob.id, bob.clone());
+    db.insert(lyall.id, lyall.clone());
+
+    // 2️⃣ READ
+    if let Some(p) = db.get(&1) {
+        println!("Read: {:?}", p);
+    }
+
+    // 3️⃣ UPDATE
+    if let Some(mut p) = db.get_mut(&2) {
+        p.age = 26;
+        println!("Updated: {:?}", *p);
+    }
+
+    // 4️⃣ DELETE
+    db.remove(&1);
+    println!(
+        "Remaining IDs: {:?}",
+        db.iter().map(|r| *r.key()).collect::<Vec<_>>()
+    );
+
+    // 5️⃣ QUERY (manual "WHERE age > 25")
+    let results: Vec<Person> = db
+        .iter()
+        .filter_map(|entry| {
+            let person = entry.value();
+            if person.name.eq("Lyall") {
+                Some(person.clone())
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    println!("Query name = Alice: {:#?}", results);
+}
+*/
---
+++ src/modules/etf.rs
@@ -0,0 +1,27 @@
+// method to return a list of ETFs from a CSV file
+use csv::ReaderBuilder;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Etf {
+    pub symbol: String,
+    pub name: String,
+    pub currency: String,
+    pub summary: String,
+    pub category_group: String,
+    pub category: String,
+    pub family: String,
+    pub exchange: String,
+}
+
+pub fn read_etfs_from_csv(file_path: &str) -> Result<Vec<Etf>, Box<dyn std::error::Error>> {
+    let mut rdr = ReaderBuilder::new().from_path(file_path)?;
+    let mut etfs = Vec::new();
+
+    for result in rdr.deserialize() {
+        let etf: Etf = result?;
+        etfs.push(etf);
+    }
+
+    Ok(etfs)
+}
